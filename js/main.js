9a7a47f91956a6d48423fba2751fc6ce7666f338
ahmedEssamFawaz <49008995+ahmedEssamFawaz@users.noreply.github.com>
2021-01-19 23:14:56 +0200

    update5

--- js/main.js
+++ js/main.js
@@ -75,72 +75,117 @@ function getVals(){
     });
   });
   
-  $(document).ready(function(){
-    $(".show-36").click(function(){
-      $(".s-36").css("display", "block");
-      // $(".s-37,.s-38,.s-39,.s-3xl,.s-40,.s-41,.s-42,.s-43,.s-44,.s-xs").css("display", "none");
-    });
+  // $(document).ready(function(){
+  //   $(".show-36").click(function(){
+  //     $(".s-36").css("display", "block");
+  //     // $(".s-37,.s-38,.s-39,.s-3xl,.s-40,.s-41,.s-42,.s-43,.s-44,.s-xs").css("display", "none");
+  //   });
+  // });
+  // $(document).ready(function(){
+  //   $(".show-37").click(function(){
+  //     $(".s-37").css("display", "block");
+  //     // $(".s-36,.s-38,.s-39,.s-3xl,.s-40,.s-41,.s-42,.s-43,.s-44,.s-xs").css("display", "none");
+  //   });
+  // });
+  
+  // $(document).ready(function(){
+  //   $(".show-38").click(function(){
+  //     $(".s-38").css("display", "block");
+  //   });
+  // });
+  
+  // $(document).ready(function(){
+  //   $(".show-39").click(function(){
+  //     $(".s-39").css("display", "block");
+  //   });
+  // });
+  
+  // $(document).ready(function(){
+  //   $(".show-3xl").click(function(){
+  //     $(".s-3xl").css("display", "block");
+  //   });
+  // });
+  
+  // $(document).ready(function(){
+  //   $(".show-40").click(function(){
+  //     $(".s-40").css("display", "block");
+  //   });
+  // });
+  
+  // $(document).ready(function(){
+  //   $(".show-41").click(function(){
+  //     $(".s-41").css("display", "block");
+  //   });
+  // });
+  
+  // $(document).ready(function(){
+  //   $(".show-42").click(function(){
+  //     $(".s-42").css("display", "block");
+  //   });
+  // });
+  
+  // $(document).ready(function(){
+  //   $(".show-43").click(function(){
+  //     $(".s-43").css("display", "block");
+  //   });
+  // });
+  
+  // $(document).ready(function(){
+  //   $(".show-44").click(function(){
+  //     $(".s-44").css("display", "block");
+  //   });
+  // });
+  
+  // $(document).ready(function(){
+  //   $(".show-xs").click(function(){
+  //     $(".s-xs").css("display", "block");
+  //   });
+  // });
+  
+  
+  // ADD COLOR IDS IN ARRAY 
+  var colors = ['s-36','s-37','s-38','s-39','s-3xl','s-40','s-41','s-42','s-43','s-44','s-xs'];
+
+  // Variable which keeps track of which color is currently visible
+var visibleColors = null;
+
+function onLinkClicked(event) {
+  console.log(event);
+  // Get the id of the link that was clicked.
+  var linkName = event.currentTarget.className;
+  console.log(linkName);
+
+  // Strip off the '-link' from the end of the linkName
+  var dashIndex = linkName.indexOf('-link');
+  console.log(dashIndex);
+  var appName = linkName.substr(0, dashIndex);
+  console.log(appName);
+
+  // Call show app with the correct appName.
+  console.log(5+5);
+  showApp(appName);
+}
+
+
+function showApp(appNameToShow) { 
+  console.log(5+5);
+  // Hide the currently visible app (if there is one!)
+  if (visibleColors !== null) {
+      $('#' + visibleColors).hide();
+  }
+
+  // And show the one passed
+  $('#' + appNameToShow).show();
+
+  // Update the visibleApp property.
+  visibleColors = appNameToShow;
+}
+
+// $(document).ready waits for the page to finish rendering
+$(document).ready(function () {
+  // Walk through the Array of Apps and add a click handler to
+  // it's respective link.
+  colors.forEach(function(name) {
+      $('.' + name + '-link').on('click', onLinkClicked);
   });
-  $(document).ready(function(){
-    $(".show-37").click(function(){
-      $(".s-37").css("display", "block");
-      // $(".s-36,.s-38,.s-39,.s-3xl,.s-40,.s-41,.s-42,.s-43,.s-44,.s-xs").css("display", "none");
-    });
-  });
-  
-  $(document).ready(function(){
-    $(".show-38").click(function(){
-      $(".s-38").css("display", "block");
-    });
-  });
-  
-  $(document).ready(function(){
-    $(".show-39").click(function(){
-      $(".s-39").css("display", "block");
-    });
-  });
-  
-  $(document).ready(function(){
-    $(".show-3xl").click(function(){
-      $(".s-3xl").css("display", "block");
-    });
-  });
-  
-  $(document).ready(function(){
-    $(".show-40").click(function(){
-      $(".s-40").css("display", "block");
-    });
-  });
-  
-  $(document).ready(function(){
-    $(".show-41").click(function(){
-      $(".s-41").css("display", "block");
-    });
-  });
-  
-  $(document).ready(function(){
-    $(".show-42").click(function(){
-      $(".s-42").css("display", "block");
-    });
-  });
-  
-  $(document).ready(function(){
-    $(".show-43").click(function(){
-      $(".s-43").css("display", "block");
-    });
-  });
-  
-  $(document).ready(function(){
-    $(".show-44").click(function(){
-      $(".s-44").css("display", "block");
-    });
-  });
-  
-  $(document).ready(function(){
-    $(".show-xs").click(function(){
-      $(".s-xs").css("display", "block");
-    });
-  });
-  
-  
-  
+});    
